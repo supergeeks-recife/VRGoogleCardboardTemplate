ad5b9ccec37cb6ec6512c3f9491345f84315a1a4
kleberkgns <kleber.silva.kgns@gmail.com>
2023-06-17 16:34:51 -0300

    ArduinoIntagration script

---
+++ Assets/Scripts/ArduinoIntegration.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoIntegration : MonoBehaviour
+{
+    public string portName = "COM4"; // Porta serial do Arduino
+    public int baudRate = 9600; // Taxa de transmissão
+
+    private SerialPort serialPort;
+
+    private void Start()
+    {
+        serialPort = new SerialPort(portName, baudRate);
+        serialPort.Open();
+    }
+
+    private void Update()
+    {
+        if (serialPort.IsOpen)
+        {
+            string message = serialPort.ReadLine();
+
+            if(Input.GetKeyDown(KeyCode.Space))
+                Debug.Log("Botão do Arduino pressionado");
+
+            /*
+            if (message.Contains("Botão pressionado"))
+            {
+                Debug.Log("Botão do Arduino pressionado");
+            }
+            */
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.Close();
+        }
+    }
+}
